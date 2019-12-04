8d19a3b6746314184239966e8e9f8f7da3a2d4ce
Yuri Young <yuri.young@qq.com>
2019-12-04 21:02:25 +0800

    commit TreeModel

---
+++ src/treeview/treemodel.h
@@ -0,0 +1,40 @@
+/**
+ * QML examples - Qt5 and QML examples
+ * Copyright (c) 2019 Yuri Young<yuri.young@qq.com>
+ *
+ * This examples is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This examples is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @date 2019/12/4-12-4
+ * @filename treemodel.h
+ * @class TreeModel
+ * @author Yuri Young<yuri.young@qq.com>
+ * @qq 12319597
+ */
+#ifndef TREEMODEL_H
+#define TREEMODEL_H
+
+#include <QObject>
+
+class TreeModel : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TreeModel(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // TREEMODEL_H
